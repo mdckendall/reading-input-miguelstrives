a8654cb82119950050accfe941c3c6caa0fea34a
Miguel Piedra <124907845+miguelstrives@users.noreply.github.com>
2023-02-27 22:45:45 +0000

    reading input java

--- Main.java
+++ Main.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Enter a word:");
-    
-    //Your code will return the length of the entered String
+    Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a word: ");
+        String word = input.next();
+
+        int length = word.length();
+        System.out.println("The length of the word is: " + length);
   }
 }
